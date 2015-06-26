175645a56b5ac146d642a369248395d84945ca6c
Nattaphoom Ch <attomos@gmail.com>
2015-06-26 23:47:34 +0700

    Created OutputBox as a separate component.
    Added README file.

---
+++ README.md
@@ -0,0 +1,4 @@
+# You can simply ignore this #
+
+My mom just want me to write a program for her
+[combination](https://en.wikipedia.org/wiki/Combination) thing.
--- js/app.jsx
+++ js/app.jsx
@@ -2,49 +2,56 @@ var Combinatorics = require('js-combinatorics');
 var React = require('react/addons');
 
 var MomWinApp = React.createClass({
-  mixins: [React.addons.LinkedStateMixin],
   handleKeyUp: function(e) {
-    var input = document.getElementById('id_input');
-    var value = input.value;
-    var v = parseInt(value, 10) || 0;
-    if (v === 0 || v < 10) {
-      this.setState({output2: '', output3: ''});
-      return;
-    }
-    var array = value.split('');
-    // Get all 2-digit combinations.
-    var cmb2 = Combinatorics.combination(array, 2);
-    var output2 = '';
-    var a = cmb2.next();
-    while(a) {
-      output2 += a.join('') + " ";
-      a = cmb2.next();
-    }
-    this.setState({output2: output2.trim()});
-    if (v < 100) {
-      return;
-    }
-    // Get all 3-digit combinations.
-    var cmb3 = Combinatorics.combination(array, 3);
-    var output3 = '';
-    var b = cmb3.next();
-    while(b) {
-      output3 += b.join('') + " ";
-      b = cmb3.next();
-    }
-    this.setState({output3: output3.trim()});
+    var value = document.getElementById('id_input').value;
+    this.setState({value: value});
+    this.refs.output2.updateOutput(value);
+    this.refs.output3.updateOutput(value);
   },
   getInitialState: function() {
-    return {output2: '', output3: ''};
+    return {value: 0};
   },
   render: function() {
     return (
       <div>
-      <input id="id_input" className="input u-full-width" type="text" maxLength="10" autoFocus onKeyUp={this.handleKeyUp}/>
-      <h3>2 ตัว</h3>
-      <textarea className="output-2" rows="15" valueLink={this.linkState('output2')}></textarea>
-      <h3>3 ตัว</h3>
-      <textarea className="output-3" rows="12" valueLink={this.linkState('output3')}></textarea>
+        <input id="id_input" className="input u-full-width" type="text" maxLength="10" autoFocus onChange={this.handleKeyUp} />
+        <OutputBox id="output-2" ref="output2" />
+        <OutputBox id="output-3" ref="output3" />
+      </div>
+    );
+  }
+});
+
+var OutputBox = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+  updateOutput: function(value) {
+    var data = value
+    var v = parseInt(data,  10) || 0;
+    // to prevent out of range exception in combination function
+    if (v < Math.pow(10, this.state.N - 1)) {
+      return this.setState({output: ''});
+    }
+    var cmb = Combinatorics.combination(data.split(''), this.state.N);
+    var output = '';
+    var a = cmb.next();
+    while(a) {
+      output += a.join('') + " ";
+      a = cmb.next();
+    }
+    this.setState({output: output.trim()});
+  },
+  getInitialState: function() {
+    return {N: 0, output: ''};
+  },
+  componentDidMount: function() {
+    var N = parseInt(this.props.id.split('-')[1], 10);
+    this.setState({N: N});
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>{this.state.N} ตัว</h3>
+        <textarea id={this.props.id} rows="12" valueLink={this.linkState('output')}></textarea>
       </div>
     );
   }
